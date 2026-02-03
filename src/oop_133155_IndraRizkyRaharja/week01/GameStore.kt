b9f1a6684dd211f3b0acf2d3261348ed1e8f938b
Reika48 <marlon.rizky@gmail.com>
2026-02-03 18:21:29 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_133155_IndraRizkyRaharja/week01/GameStore.kt
+++ src/oop_133155_IndraRizkyRaharja/week01/GameStore.kt
@@ -5,6 +5,21 @@ fun main() {
     val gameTitle = "Elden Ring: Shadow of the Erdtree"
     val price = 750000
 
+    // Perhitungan diskon
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
     println("Game: $gameTitle")
-    println("Price: Rp $price")
+    println("Harga Asli: Rp $price")
+    println("Diskon: Rp $discount")
+    println("Harga Akhir: Rp $finalPrice")
+}
+
+// Logika Diskon (Expression Body)
+fun calculateDiscount(price: Int): Int {
+    return if (price > 500000) {
+        (price * 0.2).toInt()
+    } else {
+        (price * 0.1).toInt()
+    }
 }
