a9b085be4a90be5a0638bc2e7c5d32dbee01683a
Reika48 <marlon.rizky@gmail.com>
2026-02-03 15:17:40 +0700

    week01: initial setup and procedural code

---
+++ src/oop_133155_IndraRizkyRaharja/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_133155_IndraRizkyRaharja.week01
+
+fun main() {
+    var name: String = "IndraRizkyRaharja"
+    var score: Int = 100
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
