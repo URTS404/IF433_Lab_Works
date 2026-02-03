075fd7318faf1a0f14fa7f193f68ce461cf62334
Reika48 <marlon.rizky@gmail.com>
2026-02-03 18:18:28 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_133155_IndraRizkyRaharja/week01/GameStore.kt
@@ -0,0 +1,10 @@
+package oop_133155_IndraRizkyRaharja.week01
+
+fun main() {
+    // Persiapan Data
+    val gameTitle = "Elden Ring: Shadow of the Erdtree"
+    val price = 750000
+
+    println("Game: $gameTitle")
+    println("Price: Rp $price")
+}
