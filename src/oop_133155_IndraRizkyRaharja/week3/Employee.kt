23f4a8cffdbf019d0bb1393d579d5b7ec0adf3b2
Reika48 <marlon.rizky@gmail.com>
2026-02-20 15:15:36 +0700

    week03: add private property

--- src/oop_133155_IndraRizkyRaharja/week3/Employee.kt
+++ src/oop_133155_IndraRizkyRaharja/week3/Employee.kt
@@ -1,13 +1,28 @@
 package oop_133155_IndraRizkyRaharja.week3
 
 class Employee(val name: String) {
+
     var salary: Int = 0
         set(value) {
             if (value < 0) {
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0 // Gunakan field, bukan this.salary
+                field = 0
             } else {
-                field = value // Gunakan field untuk assgin nilai asli
+                field = value // Gunakan field untuk assign nilai asli
             }
         }
+
+    // Hanya bisa di akses di file/class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformanceRating() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
+    // Kecuali kita buat function khusus untuk print.
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
