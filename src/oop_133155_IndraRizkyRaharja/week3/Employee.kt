fb5966acbe872d0b0a0561439fea074063fbe362
Reika48 <marlon.rizky@gmail.com>
2026-02-20 15:00:58 +0700

    week03: simulate stack overflow error in setter

--- src/oop_133155_IndraRizkyRaharja/week3/Employee.kt
+++ src/oop_133155_IndraRizkyRaharja/week3/Employee.kt
@@ -2,4 +2,10 @@ package oop_133155_IndraRizkyRaharja.week3
 
 class Employee(val name: String) {
     val salary: Int = 0
+        set (value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
+            // Kita memanggil setter di dalam setter (Recursive)
+            this.salary = value
+        }
 }
