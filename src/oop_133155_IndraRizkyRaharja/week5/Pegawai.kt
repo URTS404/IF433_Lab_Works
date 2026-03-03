05ecea52db304c34cc4d9c56f2650b391f3e6bb7
Reika48 <marlon.rizky@gmail.com>
2026-03-03 21:00:16 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_133155_IndraRizkyRaharja/week5/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_133155_IndraRizkyRaharja.week5
+
+abstract class Pegawai(val name: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
