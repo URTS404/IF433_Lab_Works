d90ab5171b22620d5e1703e4767ca6c34a6242e7
Reika48 <marlon.rizky@gmail.com>
2026-03-10 15:34:48 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_133155_IndraRizkyRaharja/week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_133155_IndraRizkyRaharja.week6
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
