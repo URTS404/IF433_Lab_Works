4b6b9beec6a266d44bf3bb1b25ad08509847718c
Reika48 <marlon.rizky@gmail.com>
2026-03-17 23:29:45 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_133155_IndraRizkyRaharja/week7/Main.kt
+++ src/oop_133155_IndraRizkyRaharja/week7/Main.kt
@@ -4,5 +4,13 @@ fun main() {
     GameManager.startGame()
     GameManager.startGame()
     println()
+
+    println("[ENUM] Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("[FORGE] Senjata awal ditempa:")
+    println("        Nama      : ${starterWeapon.item.name}")
+    println("        Damage    : ${starterWeapon.item.damage}")
+    println("        Rarity    : ${starterWeapon.item.rarity}")
+    println("        Durability: ${starterWeapon.durability}")
 }
 
