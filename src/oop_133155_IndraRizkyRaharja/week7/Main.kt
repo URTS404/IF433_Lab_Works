8d4d1edc6dd06427194be476fc74a38453af8f85
Reika48 <marlon.rizky@gmail.com>
2026-03-17 23:31:29 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_133155_IndraRizkyRaharja/week7/Main.kt
+++ src/oop_133155_IndraRizkyRaharja/week7/Main.kt
@@ -12,5 +12,15 @@ fun main() {
     println("        Damage    : ${starterWeapon.item.damage}")
     println("        Rarity    : ${starterWeapon.item.rarity}")
     println("        Durability: ${starterWeapon.durability}")
-}
 
+    println()
+    val upgradedItem = starterWeapon.item.copy(damage = 5)
+    println("[BLACKSMITH] Senjata diupgrade! Damage baru: ${upgradedItem.damage}")
+
+    println()
+    println("=== SIMULASI BATTLE EVENT ===")
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Hijau Tambang"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena keracunan MBG"))
+}
