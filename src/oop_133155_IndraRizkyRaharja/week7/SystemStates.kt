57183adf0c08013ab4cd7e51cd55e07c6bf91a86
Reika48 <marlon.rizky@gmail.com>
2026-03-17 16:36:32 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_133155_IndraRizkyRaharja/week7/SystemStates.kt
+++ src/oop_133155_IndraRizkyRaharja/week7/SystemStates.kt
@@ -1,11 +1,11 @@
 package oop_133155_IndraRizkyRaharja.week7
 
-enum class AppState {
-    STARTING, RUNNING, STOPPED
-}
-
 sealed class ApiResponse {
-    data class Success(val data: DataUser) : ApiResponse()
+    data class Success(val data: String) : ApiResponse()
     data class Error(val error: String) : ApiResponse()
     object Loading : ApiResponse()
+}
+
+enum class AppState {
+    STARTING, RUNNING, STOPPED
 }
