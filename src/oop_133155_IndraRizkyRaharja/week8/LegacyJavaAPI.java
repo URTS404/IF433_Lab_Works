4618df5b475f5a3d3e888f792950f74c791f9fc3
Reika48 <marlon.rizky@gmail.com>
2026-04-14 16:17:40 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_133155_IndraRizkyRaharja/week8/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_133155_IndraRizkyRaharja.week8;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
