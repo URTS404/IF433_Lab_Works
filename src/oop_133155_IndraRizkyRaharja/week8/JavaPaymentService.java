a6c577ad3cc5ec3b4ccf5e0b5e2299f4e181f540
Reika48 <marlon.rizky@gmail.com>
2026-04-14 16:27:38 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_133155_IndraRizkyRaharja/week8/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_133155_IndraRizkyRaharja.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        // TODO: Boleh modifikasi format Transaction ID sesuai kebutuhan
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
