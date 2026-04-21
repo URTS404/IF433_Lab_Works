654f7e148eee79789703c94f9f4ad46dd648dc91
Reika48 <marlon.rizky@gmail.com>
2026-04-21 12:49:46 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_133155_IndraRizkyRaharja/week9/TradingDashboard.kt
+++ src/oop_133155_IndraRizkyRaharja/week9/TradingDashboard.kt
@@ -17,4 +17,7 @@ fun main() {
         .filter { it.roe > 0 }
     val losingTrades = closedTrades
         .filter { it.roe <= 0 }
+    val topPerformersString = winningTrades
+        .sortedByDescending { it.roe }
+        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
 }
