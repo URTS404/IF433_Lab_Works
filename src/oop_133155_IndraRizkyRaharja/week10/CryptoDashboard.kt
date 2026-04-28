6e31839ba8c54e692b4b69d69fa2fee2e6c0f012
Reika48 <marlon.rizky@gmail.com>
2026-04-28 07:54:14 +0700

    week10: (task) populate coin repository

--- src/oop_133155_IndraRizkyRaharja/week10/CryptoDashboard.kt
+++ src/oop_133155_IndraRizkyRaharja/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_133155_IndraRizkyRaharja.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("Dogecoin",    2.500))
+    coinRepo.add(Coin("MemeCore",   10.000))
+    coinRepo.add(Coin("wojak",       4000.0))
 }
