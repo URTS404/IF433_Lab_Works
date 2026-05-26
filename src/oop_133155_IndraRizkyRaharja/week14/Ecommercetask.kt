e951f6a41ece9db62415026ed9558bdf547b937e
Reika48 <marlon.rizky@gmail.com>
2026-05-26 08:14:37 +0700

    week14: (task) copy bad E-Commerce OrderProcessor code

--- src/oop_133155_IndraRizkyRaharja/week14/Ecommercetask.kt
+++ src/oop_133155_IndraRizkyRaharja/week14/Ecommercetask.kt
@@ -1,2 +1,25 @@
 package oop_133155_IndraRizkyRaharja.week14
+import java.io.File
 
+class BadOrderProcessor {
+    // VIOLATION: Hardcoded File I/O (DIP), Melakukan + I/O + Notifikasi sekali
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+
+        // VIOLATION OCP: kaku jika ada tipe customer/diskon baru di masa depan (OCP)
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP"     -> basePrice * 0.90  // Diskon 10%
+            else      -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        // VIOLATION SRP/DIP: Menulis file langsung di class bisnis
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+
+        // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
